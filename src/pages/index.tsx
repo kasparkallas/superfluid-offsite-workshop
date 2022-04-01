aa70c24a2846ad70e1eb9ccbd8bc1a9394e5f1cc
Kaspar Kallas <kaspar@superfluid.finance>
2022-04-01 10:52:54 +0300

    Add links to docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,22 @@ const Home: NextPage = () => {
           alignItems: 'center',
         }}
       >
-        Workshop
+        <h1>Workshop</h1>
+        <a href="https://sdk-redux-refs.netlify.app/" target="_blank">
+          sdk-redux reference docs
+        </a>
+        <a
+          href="https://refs.superfluid.finance/sdk-core@0.3.2/"
+          target="_blank"
+        >
+          sdk-core reference docs
+        </a>
+        <a
+          href="https://redux-toolkit.js.org/rtk-query/overview"
+          target="_blank"
+        >
+          rtk-query docs
+        </a>
       </Box>
     </Container>
   )
