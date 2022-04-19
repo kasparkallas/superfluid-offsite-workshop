58646ae759fdabfb4af1e53d7413f46ff85f0639
Kaspar Kallas <kaspar@superfluid.finance>
2022-04-19 17:48:29 +0300

    Add subgraph search example

--- src/pages/subgraph.tsx
+++ src/pages/subgraph.tsx
@@ -1,9 +1,31 @@
-import * as React from 'react'
 import type { NextPage } from 'next'
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
+import { subgraphApi } from '../redux/store'
+import { List, ListItem, ListItemText, Typography } from '@mui/material'
+import { useNetworkContext } from '../contexts/NetworkContext'
+import { useWalletContext } from '../contexts/WalletContext'
+import FlowingBalance from '../components/FlowingBalance'
+import { skipToken } from '@reduxjs/toolkit/dist/query'
 
 const Subgraph: NextPage = () => {
+  const { network } = useNetworkContext()
+  const { walletAddress } = useWalletContext()
+
+  const accountTokenSnapshotsQuery = subgraphApi.useAccountTokenSnapshotsQuery(
+    walletAddress
+      ? {
+          chainId: network.chainId,
+          filter: {
+            account: walletAddress,
+          },
+          pagination: {
+            take: Infinity,
+          },
+        }
+      : skipToken
+  )
+
   return (
     <Container maxWidth="lg">
       <Box
@@ -15,7 +37,34 @@ const Subgraph: NextPage = () => {
           alignItems: 'center',
         }}
       >
-        Subgraph
+        <Typography variant="h1">Subgraph</Typography>
+        <Typography variant="subtitle1" sx={{ mb: 3 }}>
+          Account Token Balances
+        </Typography>
+        {!walletAddress
+          ? 'Connect wallet, plz'
+          : accountTokenSnapshotsQuery.isUninitialized ||
+            accountTokenSnapshotsQuery.isLoading
+          ? 'Loading...'
+          : ''}
+        {accountTokenSnapshotsQuery.data && (
+          <List>
+            {accountTokenSnapshotsQuery.data.items.map((x) => (
+              <ListItem key={x.id}>
+                <ListItemText
+                  primary={x.tokenSymbol}
+                  secondary={
+                    <FlowingBalance
+                      balance={x.balanceUntilUpdatedAt}
+                      flowRate={x.totalNetFlowRate}
+                      balanceTimestamp={x.updatedAtTimestamp}
+                    />
+                  }
+                ></ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        )}
       </Box>
     </Container>
   )
